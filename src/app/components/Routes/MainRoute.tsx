27dcd765f5f6e3cf5bbb38a628b14a48d4ffb350
keka3avr <kekazavr228@gmail.com>
2022-11-13 01:03:32 +1100

    mobile styles implemented

--- src/app/components/Routes/MainRoute.tsx
+++ src/app/components/Routes/MainRoute.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MainRoute() {
     return (
-        <section className='main main-auth'>
+        <section className='main main-app'>
             <header className='header header-app framed'>
                 <div className='title-container'>
                     <span className='title title-name text--block'>Monica Belucci</span>
@@ -12,15 +12,128 @@ export default function MainRoute() {
                     Log Out
                 </button>
             </header>
-            <main className='content framed'>
-                <div className='title-container'>
-                    <span className='title'>Good Afternoon !</span>
-                </div>
+            <main className='content content-app framed'>
+                <span className='title title-time'>Good Afternoon !</span>
+
                 <form className='form form-tasks'>
+                    <span className='form-tasks__title'>Tasks List</span>
                     <div className='form-tasks__container'>
                         <header className='form-tasks__header'>
-                            <span className='form-tasks__title'>Add new task</span>
+                            <span className='form-tasks__header-desc'>Add new task</span>
+                            <button type='button' className='btn btn-add'>
+                                <img
+                                    src='./svg/plus.svg'
+                                    alt='add-icon'
+                                    className='svg svg-add'
+                                />
+                            </button>
                         </header>
+                        <ul className='todos'>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                            <li className='todo'>
+                                <input className='todo__checkbox' type='checkbox' />
+                                <span className='todo__desc'>
+                                    Cook Rice and Chicken at 10 am
+                                </span>
+                                <button type='button' className='btn btn-remove'>
+                                    <img
+                                        src='./svg/remove.svg'
+                                        alt='remove-icon'
+                                        className='svg svg-remove'
+                                    />
+                                </button>
+                            </li>
+                        </ul>
                     </div>
                 </form>
             </main>
