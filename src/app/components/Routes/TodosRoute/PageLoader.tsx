d1b24aac7c8df9859845a8431d345822314a0734
keka3avr <kekazavr228@gmail.com>
2022-11-19 01:36:06 +1100

    pageLoader styles refactored

--- src/app/components/Routes/TodosRoute/PageLoader.tsx
+++ src/app/components/Routes/TodosRoute/PageLoader.tsx
@@ -4,8 +4,8 @@ type PageLoaderProps = { cls?: string; desc: string };
 
 export default function PageLoader({ cls, desc }: PageLoaderProps) {
     return (
-        <div className={`page-loader-container ${cls}`}>
-            <span className='page-loader-desc'>{desc}</span>
+        <div className={`page-loader__container ${cls}`}>
+            <span className='page-loader__desc'>{desc}</span>
             <div className='animation-area'>
                 <div className='train-container' />
                 <div className='bg-loader bg-before' />
